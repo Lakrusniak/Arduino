24a2f2969e65f36caa251c13aaeb776cdc8c371f
Landon Krusniak <Lakrusniak@gnail.com>
2022-12-14 05:29:37 -0800

    Added  Serial.println("All clear");

--- sketch_dec12a/sketch_dec12a.ino
+++ sketch_dec12a/sketch_dec12a.ino
@@ -36,6 +36,7 @@ void loop() {
  
   if (distanceincm < 11) {
       digitalWrite(LEDlampGreen, HIGH);
+       Serial.println("Getting close ");
 }
   else {
       digitalWrite(LEDlampGreen, LOW);
@@ -56,7 +57,7 @@ void loop() {
   }
  
   if (distanceincm > 10 || distanceincm <= 0){
-    Serial.println("Outside the permissible range of distances");
+    Serial.println("All clear");
     noTone(soundBuzzer);
   }
   else {
